ee6551134f83591e1c1e0e97cb873651dd5f75f9
ksc5496 <ksc5496@psu.edu>
2020-09-10 16:58:30 +0000

    left run brackets empty.

---
+++ main.py
@@ -0,0 +1,36 @@
+# Author: Krish Choudhary ksc5496@psu.edu
+# Collaborator: Kieran Murdocca kkm5574@psu.edu 
+# Collaborator: Reuben Lee wzl128@psu.edu
+# Collaborator: Augustus Perseghin agp5191@psu.edu
+# Section: 4
+# Breakout: 18
+
+def getletterGrade(percentage):
+  if (percentage >= 93.0):
+    return "A"
+  elif (percentage >= 90.0):
+    return "A-"
+  elif (percentage >= 87.0):
+    return "B+"
+  elif (percentage >= 83.0):
+    return "B"
+  elif (percentage >= 80.0):
+    return "B-"
+  elif (percentage >= 77.0):
+    return "C+"
+  elif (percentage >= 70.0):
+    return "C"
+  elif (percentage >= 60.0):
+    return "D"
+  else: 
+    return "F"
+    
+
+def run():
+  percentage = input("Enter your CMPSC 131 grade: ")
+  percentage = float(percentage)
+  print("Your letter grade for CMPSC 131 is " + str(getletterGrade) + ".")
+
+if __name__ == "__main__":
+  run()
+ 
